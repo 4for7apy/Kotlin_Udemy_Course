56b930b60c36392ce3a7cf325455b5f6a9cd8915
arpit <arpit88407@gmail.com>
2022-12-30 12:22:25 +0530

    Added Book2

--- src/main/kotlin/Udemy13.kt
+++ src/main/kotlin/Udemy13.kt
@@ -14,7 +14,10 @@ data class Databook(val title: String , val author: String , val price: Int){
 fun main(args: Array<String>){
     val Book = book("IKIGAI" , "Bhhoot" ,500 )
     val databook = Databook("The Pyscology of money" , "Bhoot2" , 4000)
+    val Book2 = book("Rich dad poor dad" , "Bhoot3" , 1500)
+
 
     println(Book)
     println(databook)
+    println(Book2)
 }
