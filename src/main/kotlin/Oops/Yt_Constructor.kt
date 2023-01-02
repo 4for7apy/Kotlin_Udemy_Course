e246b839b895ff5e3219812812233114a1432eca
arpit <arpit88407@gmail.com>
2023-01-02 20:38:50 +0530

    Added Constructor In Oops

--- src/main/kotlin/Oops/Yt_Constructor.kt
+++ src/main/kotlin/Oops/Yt_Constructor.kt
@@ -1,2 +1,20 @@
-package Oops
 
+//Constructor
+class Students(var name: String , var age: Int){
+    fun paste(){
+        println("Name of the animal is '$name' , Age of the animal is '$age'")
+    }
+
+}
+fun main(args: Array<String>){
+    var students1 = Students("Arpit" , 18)
+    students1.paste()
+    var students2 = Students("Arpita" , 15)
+    students2.paste()
+    var students3 = Students("Anil" , 18)
+    students3.paste()
+    var students4 = Students("Bhoot" , 20)
+    students4.paste()
+
+
+}
