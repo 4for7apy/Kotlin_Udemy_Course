d5fb2e4d48c5d1390d81afba4a4e012da984d528
arpit <arpit88407@gmail.com>
2023-01-03 14:20:47 +0530

    Added Constructor In Oops

---
+++ src/main/kotlin/Oops/Yt3.kt
@@ -0,0 +1,25 @@
+/*
+class Apple
+{
+    var size: Int //Global variable
+    var Price : Int
+    constructor( size: Int, Price : Int)//Local Variable
+    {
+        this.size = size
+        this.Price = Price    // This is used to give the value of local variable to gloabal variable
+    }
+    fun Appledetails(){
+        println("Size of apple = $size")
+        println("Price of apple is = $Price")
+    }
+fun main(args:Array<String>){
+    var apple1 = Apple(21 , 20)
+    apple1.Appledetails()
+
+}
+
+
+}
+
+
+ */
--- src/main/kotlin/Oops/Yt_Constructor.kt
+++ src/main/kotlin/Oops/Yt_Constructor.kt
@@ -1,8 +1,16 @@
-
-//Constructor
+import kotlin.system.exitProcess
+/*
+//PRIMARY Constructor
 class Students(var name: String , var age: Int){
+
+    init {      //init is used to add dataTypes in Class
+        val Supername:String
+        Supername = name
+        println("Bhoot")
+    }
     fun paste(){
         println("Name of the animal is '$name' , Age of the animal is '$age'")
+
     }
 
 }
@@ -18,3 +26,37 @@ fun main(args: Array<String>){
 
 
 }
+
+*/
+
+/*
+//SECONDARY CONSTRUCTER
+
+class Apple
+{
+    var size: Int //Global variable
+    var Price : Int
+    constructor( size: Int, Price : Int)//Local Variable
+    {
+        this.size = size
+        this.Price = Price    // This is used to give the value of local variable to gloabal variable
+    }
+    fun Appledetails(){
+        println("Size of apple = $size")
+        println("Price of apple is = $Price")
+    }
+
+fun main(args: Array<String>){
+    var apple1 = Apple(20 , 50)
+    var apple2 = Apple(30 , 40)
+
+    apple1.Appledetails()
+    apple2.Appledetails()
+
+}
+
+
+}
+
+
+ */
